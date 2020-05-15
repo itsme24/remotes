d8506c2c06438b4af9213712f1369c91b05e8c8b
Jim Hester <james.f.hester@gmail.com>
2020-05-15 07:24:32 -0400

    Tweak argument order
    Most of the time the path will not change, so we can put it after the os
    and os_release, which always need to be specified

--- R/system_requirements.R
+++ R/system_requirements.R
@@ -1,17 +1,17 @@
-DEFAULT_RSPM_REPO_ID <-  "2" # cran
-DEFAULT_RSPM <-  "https://packagemanager.rstudio.com"
+DEFAULT_RSPM_REPO_ID <-  "12" # cran
+DEFAULT_RSPM <-  "https://solo.rstudiopm.com"
 
-#' Query the system requirements for a package (and its dependencies)
+#' Query the system requirements for a dev package (and its dependencies)
 #'
 #' Returns a character vector of system commands to run, if you want to run '
-#' @param package The package name to query. If `NULL` the package name is
-#'   looked up from a DESCRIPTION file in the current working directory.
+#' @param path The path to the dev package's root directory.
 #' @param os,os_release The operating system and operating system release version, see
 #'   <https://github.com/rstudio/r-system-requirements#operating-systems> for the
 #'   list of supported operating systems.
+#' @param curl The location of the curl binary on your system.
 #' @return A character vector of commands to run which will install the system requirements for the package.
 #' @export
-system_requirements <- function(package = NULL, os, os_release) {
+system_requirements <- function(os, os_release, path = ".", curl = Sys.which("curl")) {
   os_versions <- supported_os_versions()
 
   os <- match.arg(os, names(os_versions))
@@ -22,21 +22,17 @@ system_requirements <- function(package = NULL, os, os_release) {
   rspm_repo_id <- Sys.getenv("RSPM_REPO_ID", DEFAULT_RSPM_REPO_ID)
   rspm_repo_url <- sprintf("%s/__api__/repos/%s", rspm, rspm_repo_id)
 
-  tmp <- tempfile()
-  download(tmp, sprintf("%s/packages?name=%s", rspm_repo_url, package))
+  desc_file <- normalizePath(file.path(path, "DESCRIPTION"))
 
-  pkg_version_id <- json$parse_file(tmp)[[1]][["id"]]
+  res <- system2(curl, args = c("--silent", "--data-binary", paste0("@", desc_file), sprintf("%s/sysreqs?distribution=%s&release=%s&suggests=true", rspm_repo_url, os, os_release)), stdout = TRUE)
 
-  tmp2 <- tempfile()
-  download(tmp2, sprintf("%s/packages/%s/sysreqs?id=%s&distribution=%s&release=%s", rspm_repo_url, package, pkg_version_id, os, os_release))
-
-  res <- json$parse_file(tmp2)
+  res <- json$parse(res)
 
   pre_install <- unique(unlist(c(res[["pre_install"]], lapply(res[["dependencies"]], `[[`, "pre_install"))))
 
-  install_scripts <- unique(unlist(c(res[["install_scripts"]]), lapply(res[["dependencies"]], `[[`, "install_scripts")))
+  install_scripts <- unique(unlist(c(res[["install_scripts"]], lapply(res[["dependencies"]], `[[`, "install_scripts"))))
 
-  c(pre_install, install_scripts)
+  as.character(c(pre_install, install_scripts))
 }
 
 
@@ -49,7 +45,7 @@ supported_os_versions <- function() {
     "centos" = c("6", "7", "8"),
     "redhat" = c("6", "7", "8"),
     "opensuse" = c("42.3", "15.0"),
-    "sle" = c("12.3", "15.0"),
+    "sle" = c("12.3", "15.0")
     #"windows" = c("")
   )
 }
